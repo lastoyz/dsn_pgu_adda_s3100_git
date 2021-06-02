ef575509064290e50eac1c3859feb1fde29bc7d6
oyz <sungeun.jo@gmail.com>
2021-06-02 11:00:27 +0900

    up xdc

--- txem7310_pll__PGU_S3100/txem7310_pll.srcs/sources_1/imports/src_ref/txem7310_pll__s3100_ms__top.v
+++ txem7310_pll__PGU_S3100/txem7310_pll.srcs/sources_1/imports/src_ref/txem7310_pll__s3100_ms__top.v
@@ -18,77 +18,7 @@
 // # B216: not used
 
 ////
-module led_test (
-	input wire clk    , // system 10MHz 
-	input wire reset_n,
-	//
-	output wire [31:0] o_timestamp, // 
-	//
-	output wire o_run_led, // 
-     //
-    // flag
-	output wire valid
-	);
 
-// valid
-(* keep = "true" *) 
-reg r_valid; //{
-assign valid = r_valid;
-//
-always @(posedge clk, negedge reset_n)
-	if (!reset_n) begin
-		r_valid <= 1'b0;
-	end
-	else begin
-		r_valid <= 1'b1;
-	end	
-//}
-	
-// global time index in debugger based on 10MHz
-(* keep = "true" *) 
-reg [31:0] r_global_time_idx; //{
-//
-always @(posedge clk, negedge reset_n) begin
-	if (!reset_n) begin
-		r_global_time_idx <= 32'd0; 
-	end
-	else begin
-		//
-		r_global_time_idx <= r_global_time_idx + 1;
-		//
-		end
-end
-//
-assign o_timestamp = r_global_time_idx;
-//}
-
-reg FPGA_LED_RUN_STATUS;
-reg [23:0] FPGA_LED_RUN_cnt;
-
-wire sys_clk = clk;
-
-always @(posedge sys_clk)
-begin
-    if(reset_n == 1'b0) begin
-        FPGA_LED_RUN_STATUS     <= 1'b0;
-        FPGA_LED_RUN_cnt        <= {24{1'b0}};
-    end else begin
-        if(FPGA_LED_RUN_cnt < 24'h2625A0) begin             // DEC 2,500,000
-            FPGA_LED_RUN_STATUS     <= 1'b0;
-            FPGA_LED_RUN_cnt        <= FPGA_LED_RUN_cnt + 1;
-        end else if(FPGA_LED_RUN_cnt < 24'h4C4B40) begin    // DEC 5,000,000
-            FPGA_LED_RUN_STATUS     <= 1'b1;
-            FPGA_LED_RUN_cnt        <= FPGA_LED_RUN_cnt + 1;
-        end else begin 
-            FPGA_LED_RUN_STATUS     <= 1'b0;
-            FPGA_LED_RUN_cnt        <= {24{1'b0}};
-        end
-    end
-end
-
-assign o_run_led = FPGA_LED_RUN_STATUS;
-
-endmodule 
 
 
 /* top module integration */
@@ -3539,6 +3469,7 @@ ok_endpoint_wrapper__dummy  ok_endpoint_wrapper_inst (
 ///TODO: //-------------------------------------------------------//
 
 wire FPGA_LED_RUN_STATUS;
+
 led_test  led_test__inst (
 	.clk       (sys_clk            ), // system 10MHz 
 	.reset_n   (reset_n            ),
